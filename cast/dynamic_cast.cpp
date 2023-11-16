32d8368afbb387373d42b868f8e7017ecc72033a
lijinhong <2503899155@qq.com>
2023-11-15 22:14:59 -0800

    dynamic_cast 注释

--- cast/dynamic_cast.cpp
+++ cast/dynamic_cast.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+/*作用：将一个基类对象指针（或引用）cast到继承类指针，dynamic_cast会根据基类指针是否真正指向继承类指针来做相应处理，
+即会作出一定的判断*/
 using namespace std;
 
 class Father
@@ -28,13 +30,14 @@ int main()
 
     Father father;
     Son son;
-    // 上行转换
+    // 上行转换     子类的指针转换成基类的指针
     {
         ps = &son;
         pf = dynamic_cast<Father *>(ps);
         pf->f();
     }
-    // 下行转换
+
+    // 下行转换     基类的指针转换成子类的指针
     {
         pf = &son;
         ps = dynamic_cast<Son *>(pf);
